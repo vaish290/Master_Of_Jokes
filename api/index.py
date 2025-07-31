2c8a734545444c1f92fd89c6d0e039000f8cb91e
vairai <vairai@iu.edu>
2025-07-30 23:42:46 -0400

    Fix: Use wsadapter for Vercel WSGI support

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 # api/index.py
 
-from wsgi_adapter import handle_request
+from wsadapter import handle_request
 from wsgi import app  
 def handler(environ, start_response):
     return handle_request(app, environ, start_response)
